253c34ccd40dc2b9d5836042a09b324064bf2160
Viktor Kamei Mota <vikmotakamei@gmail.com>
2023-08-19 19:42:17 -0300

    tirando outras formas de comparadores

--- comparador de igualdade 2bits/eq_top.vhd
+++ comparador de igualdade 2bits/eq_top.vhd
@@ -3,13 +3,13 @@ use ieee.std_logic_1164.all;
 entity eq_top is
    port(
       sw  : in  std_logic_vector(3 downto 0); -- 4 switches
-      led : out std_logic_vector(0 downto 0) -- 1 red LED
+      led : out std_logic_vector(0 downto 0) -- 1 LED discreto
    );
 end eq_top;
 
 architecture struc_arch of eq_top is
 begin
-   -- instantiate 2-bit comparator
+   -- chama o comparador de 2 bits e da os valores para ele
    eq2_unit : entity work.eq2(struc_arch)
       port map(
          a    => sw(3 downto 2),
